0700d2ea434f7992da8ee79f2f524f0f4d3ffc50
TrungDT <thanhtrung.it25@gmail.com>
2018-06-19 16:28:22 +0700

    Creating routes

--- resources/assets/js/router.js
+++ resources/assets/js/router.js
@@ -1,5 +1,14 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+
+import ListingPage from '../components/ListingPage.vue';
+
 Vue.use(VueRouter);
 
-export default new VueRouter();
+export default new VueRouter({
+  mode: 'history',
+  routes: [
+    // { path: '/', component: HomePage }, //doesn't exist yet!
+    { path: '/listing/:listing', component: ListingPage },
+  ],
+});
