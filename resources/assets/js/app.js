271db43349e1ea1e43ed6e027c0a41b71198a8c4
TrungDT <thanhtrung.it25@gmail.com>
2018-06-19 16:14:39 +0700

    Installing Vue Router

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -2,8 +2,10 @@ import 'core-js/fn/object/assign';
 import Vue from 'vue';
 
 import ListingPage from '../components/ListingPage';
+import router from './router';
 
 var app = new Vue({
   el: '#app',
   render: h => h(ListingPage),
+  router,
 });
