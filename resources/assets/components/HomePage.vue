61fc17dcbed2f13e7b3e6ea9d33b3a75abf780d2
TrungDT <thanhtrung.it25@gmail.com>
2018-06-20 15:28:30 +0700

    Adding HomePage, structure data and style

--- resources/assets/components/HomePage.vue
+++ resources/assets/components/HomePage.vue
@@ -1,9 +1,26 @@
 <template>
-  <div>Vuebnb home page</div>
+  <div class="home-container">
+    <div
+      :key="country" 
+      v-for="(group, country) in listing_groups"
+      class="listing-summary-group"
+    >
+      <h1>Places in {{ country }}</h1>
+      <div class="listing-summaries">
+        <listing-summary
+          v-for="listing in group"
+          :key="listing.id"
+          :listing="listing"
+        >
+        </listing-summary>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import { groupByCountry } from '../js/helpers';
+import ListingSummary from './ListingSummary.vue';
 
 let serverData = JSON.parse(window.vuebnb_server_data);
 let listing_groups = groupByCountry(serverData.listings);
@@ -12,6 +29,34 @@ export default {
   data() {
     return { listing_groups };
   },
+  components: {
+    ListingSummary,
+  },
 };
 </script>
-
+<style>
+.home-container {
+  margin: 0 auto;
+  padding: 0 25px;
+}
+@media (min-width: 1131px) {
+  .home-container {
+    width: 1080px;
+  }
+}
+.listing-summary-group {
+  padding-bottom: 20px;
+}
+.listing-summaries {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  overflow: hidden;
+}
+.listing-summaries > .listing-summary {
+  margin-right: 15px;
+}
+.listing-summaries > .listing-summary:last-child {
+  margin-right: 0;
+}
+</style>
---
+++ resources/assets/components/ListingSummary.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="listing-summary">
+    <div class="wrapper">
+      <div class="thumnail" :style="backgroundImageStyle"></div>
+      <div class="info title">
+        <span>{{ listing.price_per_night }}</span>
+        <span>{{ listing.title }}</span>
+      </div>
+      <div class="info address">{{ listing.address }}</div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: ['listing'],
+  computed: {
+    backgroundImageStyle() {
+      return {
+        'background-image': `url("${this.listing.thumb}")`,
+      };
+    },
+  },
+};
+</script>
+<style>
+.listing-summaries {
+  flex: 0 0 auto;
+}
+.listing-summaries a {
+  text-decoration: none;
+}
+.wrapper {
+  max-width: 350px;
+  display: block;
+}
+.listing-summary .thumnail {
+  width: 350px;
+  height: 250px;
+  background-size: cover;
+  background-position: center;
+}
+.listing-summary .info {
+  color: #484848;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  letter-spacing: 0.2px;
+  word-wrap: break-word;
+}
+.listing-summary .info.title {
+  padding-top: 5px;
+  padding-top: 5px;
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 24px;
+}
+.listing-summary .info.address {
+  font-size: 14px;
+  line-height: 18px;
+}
+</style>
