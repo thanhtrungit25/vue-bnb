578555626cfaddca5cff6cc0043534249733baa8
TrungDT <thanhtrung.it25@gmail.com>
2018-06-20 11:48:32 +0700

    Adding the thumbnail

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -29,6 +29,15 @@ class ListingController extends Controller
 	}
 	public function get_home_web()
 	{
+		$collection = Listing::all(['id', 'address', 'title', 'price_per_night']);
+		$collection->transform(function($listing) {
+			$listing->thumb = asset(
+				'images/' . $listing->id . '/Image_1_thumb.jpg'
+			);
+			return $listing;
+		});
+
+		$data = collect(['listings' => $collection->toArray()]);
 		return view('app', ['data' => []]);
 	}
 }
