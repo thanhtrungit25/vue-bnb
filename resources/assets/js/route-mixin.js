58c0f93e4ed37b3054e68d5f9ffb8104861a3009
TrungDT <thanhtrung.it25@gmail.com>
2018-06-21 11:14:15 +0700

    Fix wrong typing

--- resources/assets/js/route-mixin.js
+++ resources/assets/js/route-mixin.js
@@ -4,7 +4,7 @@ function getData(to) {
   return new Promise(resolve => {
     let serverData = JSON.parse(window.vuebnb_server_data);
     if (!serverData.path || to.path !== serverData.path) {
-      axios.get(`/api#{to.path}`).then(({ data }) => {
+      axios.get(`/api${to.path}`).then(({ data }) => {
         resolve(data);
       });
     } else {
