39799f7ba6bda67b07f870bee0b2a6d63d9f8d43
TrungDT <thanhtrung.it25@gmail.com>
2018-06-21 17:18:57 +0700

    Adding carousel control to listing summary group component

--- resources/assets/components/ListingSummaryGroup.vue
+++ resources/assets/components/ListingSummaryGroup.vue
@@ -1,23 +1,33 @@
 <template>
   <div class="listing-summary-group">
     <h1>Places in {{ country }}</h1>
-    <div class="listing-summaries">
-      <listing-summary
-        v-for="listing in listings"
-        :key="listing.id"
-        :listing="listing"
-      >
-      </listing-summary>
+    <div class="listing-carousel">
+      <div class="controls">
+        <carousel-control dir="left"></carousel-control>
+        <carousel-control dir="right"></carousel-control>
+      </div>
+      <div class="listing-summaries-wrapper">
+        <div class="listing-summaries">
+          <listing-summary
+            v-for="listing in listings"
+            :key="listing.id"
+            :listing="listing"
+          >
+          </listing-summary>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 <script>
 import ListingSummary from './ListingSummary.vue';
+import CarouselControl from './CarouselControl.vue';
 
 export default {
   props: ['country', 'listings'],
   components: {
     ListingSummary,
+    CarouselControl,
   },
 };
 </script>
@@ -36,5 +46,24 @@ export default {
 }
 .listing-summaries > .listing-summary:last-child {
   margin-right: 0;
+}
+.listing-carousel {
+  position: relative;
+}
+.listing-carousel .controls {
+  display: flex;
+  justify-content: space-between;
+  position: absolute;
+  top: calc(50% - 45px);
+  left: -45px;
+  width: calc(100% + 90px);
+}
+.listing-carousel .controls .carousel-control {
+  color: #c5c5c5;
+  font-size: 1.5rem;
+  cursor: pointer;
+}
+.listing-summaries-wrapper {
+  overflow: hidden;
 }
 </style>
