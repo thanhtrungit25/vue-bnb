b179ddcc08ae20f5d4318dc06e00a8c9a4fab12d
TrungDT <thanhtrung.it25@gmail.com>
2018-06-22 14:37:08 +0700

    Installing Vuex

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -4,9 +4,11 @@ import Vue from 'vue';
 import App from '../components/App';
 import ListingPage from '../components/ListingPage';
 import router from './router';
+import store from './store';
 
 var app = new Vue({
   el: '#app',
   render: h => h(App),
   router,
+  store,
 });
