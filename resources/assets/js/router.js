dc10104d32b77f5075ae0dc1e839cf5e186b8ff4
TrungDT <thanhtrung.it25@gmail.com>
2018-06-22 17:17:41 +0700

    Moving per state into the store & handle addData to store

--- resources/assets/js/router.js
+++ resources/assets/js/router.js
@@ -1,12 +1,14 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import axios from 'axios';
+import store from './store';
 
 import ListingPage from '../components/ListingPage.vue';
 import HomePage from '../components/HomePage.vue';
 
 Vue.use(VueRouter);
 
-export default new VueRouter({
+let router = new VueRouter({
   mode: 'history',
   routes: [
     { path: '/', component: HomePage, name: 'home' },
@@ -16,3 +18,17 @@ export default new VueRouter({
     return { x: 0, y: 0 };
   },
 });
+
+router.beforeEach((to, from, next) => {
+  let serverData = JSON.parse(window.vuebnb_server_data);
+  if (!serverData || to.path !== serverData.path) {
+    axios.get(`/api${to.path}`).then(({ data }) => {
+      store.commit('addData', { route: to.name, data });
+    });
+  } else {
+    // load initial serverData
+    store.commit('addData', { route: to.name, data: serverData });
+  }
+});
+
+export default router;
