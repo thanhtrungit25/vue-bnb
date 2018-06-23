bf3c942536dcff0888891107cc7949d4eb2e51f2
TrungDT <thanhtrung.it25@gmail.com>
2018-06-23 09:49:33 +0700

    Responding auth state to toolbar links and toggleSave

--- resources/assets/js/store.js
+++ resources/assets/js/store.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import router from './router';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
@@ -11,11 +12,15 @@ export default new Vuex.Store({
   },
   mutations: {
     toggleSaved(state, id) {
-      let index = state.saved.findIndex(saved => saved === id);
-      if (index === -1) {
-        state.saved.push(id);
+      if (state.auth) {
+        let index = state.saved.findIndex(saved => saved === id);
+        if (index === -1) {
+          state.saved.push(id);
+        } else {
+          state.saved.splice(index, 1);
+        }
       } else {
-        state.saved.splice(index, 1);
+        router.push('/login');
       }
     },
     addData(state, { route, data }) {
