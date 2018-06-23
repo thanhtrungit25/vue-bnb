91fb5bc567fad2ac7831193bb18388e84d2357ff
TrungDT <thanhtrung.it25@gmail.com>
2018-06-23 07:00:50 +0700

    Add server routes for login

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,5 @@ Route::get('/', 'ListingController@get_home_web');
 Route::get('/listing/{listing}', 'ListingController@get_listing_web');
 
 Route::get('/saved', 'ListingController@get_home_web');
+
+Auth::routes();
